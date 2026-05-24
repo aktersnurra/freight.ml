f35609e873087669bf4ad639b64afc3feb53eaab
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2026-05-24 20:20:14 +0200

    refactor(state): remove vcaml dependency

--- bin/state.ml
+++ bin/state.ml
@@ -1,14 +1,9 @@
 type t = {
   mutable active_env : string option;
-  mutable response_history : string list;
   mutable env : Freight.Env.t;
 }
 
-let create () =
-  { active_env = None; response_history = []; env = Freight.Env.empty }
+let create () = { active_env = None; env = Freight.Env.empty }
 
-let set_active_env t active_env =
-  t.active_env <- active_env
-
-let remember_buffer t name =
-  t.response_history <- name :: t.response_history
+let set_active_env state env_name =
+  state.active_env <- env_name
--- bin/state.mli
+++ bin/state.mli
@@ -1,9 +1,7 @@
 type t = {
   mutable active_env : string option;
-  mutable response_history : string list;
   mutable env : Freight.Env.t;
 }
 
 val create : unit -> t
 val set_active_env : t -> string option -> unit
-val remember_buffer : t -> string -> unit
